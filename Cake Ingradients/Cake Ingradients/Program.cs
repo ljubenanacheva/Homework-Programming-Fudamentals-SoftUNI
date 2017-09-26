42d91aed033c69cb8fe841dc98813e952d231d8c
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2017-09-26 18:13:18 +0300

    Add files via upload

---
+++ Cake Ingradients/Cake Ingradients/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cake_Ingradients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingradients = Console.ReadLine();
+            int numberIngr = 0;
+            while (ingradients != "Bake!")
+            {
+                Console.WriteLine($"Adding ingredient {ingradients}.");
+                ingradients = Console.ReadLine();
+                numberIngr += 1;
+
+
+            }
+            Console.WriteLine($"Preparing cake with {numberIngr} ingredients.");
+        }
+    }
+}
